5f6fe5103880e4500c397ff18f700ddecaa62890
Endy Muhardin <endy.muhardin@gmail.com>
2018-04-18 14:01:44 +0700

    starter project

---
+++ src/main/java/id/ac/tazkia/callcenter/outboundcallcenter/OutboundCallcenterApplication.java
@@ -0,0 +1,12 @@
+package id.ac.tazkia.callcenter.outboundcallcenter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OutboundCallcenterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OutboundCallcenterApplication.class, args);
+	}
+}
